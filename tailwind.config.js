11db39050d226700fe34be39e0d8b7ca35af2337
timmeyer1 <timvameyer@gmail.com>
2024-03-01 16:15:43 +0100

    Add folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: 'rgba(24, 24, 24, 1)',
+        black_05: 'rgba(24, 24, 24, 0.5)',
+        white: 'rgba(245, 245, 245, 1)',
+        white_05: 'rgba(245, 245, 245, 0.5)',
+        white_01: 'rgba(245, 245, 245, 0.1)',
+        green: 'rgba(30, 215, 96, 1)',
+        green_08: 'rgba(30, 215, 96, 0.8)',
+        green_06: 'rgba(30, 215, 96, 0.6)',
+        green_top: 'rgba(26, 173, 78, 1)',
+      }
+    },
   },
   plugins: [],
 }
