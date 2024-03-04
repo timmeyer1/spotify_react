55b2bfcafe4d1680dbcfc28029427f22302b7747
timmeyer1 <timvameyer@gmail.com>
2024-03-04 16:50:38 +0100

    Navbar update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,6 +15,7 @@ const Sidebar = () => {
         <div className='hidden md:flex flex-col w-[240px] py-10 px-4 bg-black'>
             <img src={imgLogo} alt="Logo Spotify" className='w-full h-14 object-contain' />
             <NavLinks data={dataAlbumNav} marginTop={'mt-10'}/>
+            <NavLinks data={dataUserNav} marginTop={'mt-5'}/>
         </div>
         {/* gestion des icones pour ouvrir/fermer le menu en petit ecran*/}
         <div className='absolute md:hidden block top-6 right-3'>
@@ -30,7 +31,8 @@ const Sidebar = () => {
         {/* navbar pour la vue en dessous de 768px */}
         <div className={`z-20 absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white_01 to-black backdrop-blur-lg p-6 md:hidden smooth-transition ${mobileMenu ? 'left-0' : '-left-full'}`}>
         <img src={imgLogo} alt="Logo Spotify" className='w-full h-14 object-contain' />
-        <NavLinks data={dataAlbumNav} marginTop={'mt-10'} handleClick={() => isMobileMenu(false)} />
+        <NavLinks data={dataAlbumNav} marginTop={'mt-10'} handleClick={() => isMobilMenu(false)} />
+        <NavLinks data={dataUserNav} marginTop={'mt-5'} handleClick={() => isMobilMenu(false)} />
         </div>
     </>
  
